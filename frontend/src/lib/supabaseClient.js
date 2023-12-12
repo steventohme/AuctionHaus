eb4639161652554af880af1b80708a41d2be2f31
Steven Tohme <steventohme59@gmail.com>
2023-12-12 02:29:54 +0000

    December 11, 2023 at 9:29:54 PM EST

--- frontend/src/lib/supabaseClient.js
+++ frontend/src/lib/supabaseClient.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 
+process.env.SUPABASE_ = 'https://owlrgfnprlnaazotrtgs.supabase.co'
 export const supabase = createClient('https://owlrgfnprlnaazotrtgs.supabase.co', '<your-anon-key>')
