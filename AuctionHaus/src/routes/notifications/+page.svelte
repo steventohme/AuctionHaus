a027ec79bb3b42ad302542bca5aa3b3ee1bb7536
Steven Tohme <steventohme59@gmail.com>
2023-12-24 18:01:35 -0500

    create singular notification view route

--- AuctionHaus/src/routes/notifications/+page.svelte
+++ AuctionHaus/src/routes/notifications/+page.svelte
@@ -2,5 +2,4 @@ NOTIFICATIONS
 
 <!-- API CALLS:
     getNotifications(userID) - get all notifications for the user
-    getNotificationByID(notificationID) - get the notification by the notificationID
  -->
