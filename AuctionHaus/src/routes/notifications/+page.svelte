5f0b4eece2a5cc26ac2337d3142d9bc0604402a2
Steven Tohme <steventohme59@gmail.com>
2023-12-24 17:56:46 -0500

    add api calls outline for notifications

--- AuctionHaus/src/routes/notifications/+page.svelte
+++ AuctionHaus/src/routes/notifications/+page.svelte
@@ -1,1 +1,6 @@
 NOTIFICATIONS
+
+<!-- API CALLS:
+    getNotifications(userID) - get all notifications for the user
+    getNotificationByID(notificationID) - get the notification by the notificationID
+ -->
